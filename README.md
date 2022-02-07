f6914c1bf4d1bea1c8e18aace29b8e8c33045227
simonfevrier <simonfevrier123@gmail.com>
2022-02-07 18:32:05 -0500

    February 7, 2022 6:32 PM

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # random-quotes
 Simple node application for a class that prints random quotes.
+
+## Favorite quotion
+
+"With great power comes great responsibility."
+-Uncle Ben
---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes = [`"With great power comes great responsibility." - Uncle ben`,
+	`"Be yourself, everyone else is already taken." - Oscar Wilde`,
+	`"You only live once, but if you do it right, once is enough." - Mae West`]
+// colsole.log(Math.random())
+// colsole.log((3*Math.random()))
+// colsole.log(Math.floor((3*Math.random()))
+let index = Math.floor((quotes.length * Math.random()))
+console.log(quotes[index])
